519c7cf8b8e1417f1ba5eb13104b8eb8085736c2
Diego <diego.millanmor@gmail.com>
2017-12-11 13:42:43 +0100

    add First Exercise

---
+++ WebComponents/componente.js
@@ -0,0 +1,16 @@
+class AppSample extends HTMLElement {
+    constructor() {
+        super();
+        console.log("Creando el componente");
+        document.querySelector("#tp1");
+        // const oImport = ;
+        // const oElem = oImport.querySelector("#tp1");
+        this.attachShadow({mode: "open"})
+            .innerHTML = "<h1>Componente 1</h1>";
+            // .innerHTML = oElem;
+    }
+}
+
+// dentro de customElements pueden definirse los nombres de los componentes que creemos. En el método "define" pasamos un string con el nombre de la etiqueta que usaremos luego en el código html, y luego la clase que queramos que sea referenciada por el componente.
+customElements
+    .define("app-sample", AppSample);
